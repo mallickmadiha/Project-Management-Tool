c48b04f2f0c9ffc2d547eadaf5f09eda256742ba
Madiha Mallick <126231293+mallickmadiha@users.noreply.github.com>
2023-07-23 01:19:29 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
     port: 587,
     authentication: 'plain',
     enable_starttls_auto: true,
-    # user_name: 'mallickmadiha9031@gmail.com',
+    user_name: 'mallickmadiha9031@gmail.com',
     password: 'hzusmcoknsopesub',
     domain: 'smtp.gmail.com',
     openssl_verify_mode: 'none'
